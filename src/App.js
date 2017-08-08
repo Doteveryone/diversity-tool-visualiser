b977bc70e338f6d77a3886933652cab3da1dede7
ntlk <hi@ntlk.net>
2017-08-08 15:36:26 +0100

    Remove the unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Papa from 'papaparse';
 import ReactUploadFile from 'react-upload-file';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -31,13 +30,6 @@ class App extends Component {
 
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <ReactUploadFile options={uploadOptions} />
       </div>
     );
